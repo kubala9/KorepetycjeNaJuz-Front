78352f231584e1e80b38f23f01844459047e869e
Adam <rumpelteam@gmail.com>
2018-11-10 17:32:20 +0100

    Add simple auth module in Store. Add login method - TEST.

--- korepetycje-front/src/service.js
+++ korepetycje-front/src/service.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
 const auth = {
-
+  login: payload => axios.post('/api/Authorization/Login', payload)
 }
 
 export default {
